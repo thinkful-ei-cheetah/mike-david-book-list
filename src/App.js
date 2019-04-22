c5a5e7c4c54404294396c40485360f14fa8007d2
David Nordeen <davidnordeen@gmail.com>
2019-04-22 09:03:52 -0700

    added api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FilterSearch from '.FilterSearch/.FilterSearch';
+import ResultList from '.List/.ResultList';
 
 class App extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${USER-INPUT}+inauthor:keyes&key=AIzaSyC6R0xVVEABBqZuTLIChXn67svSgmDZikI`;
+    const options = {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          books: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>
+          Google Book Search
+      </header>
+        <FilterSearch />
+        <ResultList />
+        SOMETHING
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/List/ResultList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ResultList extends Component {
+  render() {
+    return (
+      <section>
+      <h2>TITLE</h2>
+      <img alt="book cover"></img>
+      <h3>AUTHOR</h3>
+      <h3>PRICE</h3>
+      <p>DESCRIPTION</p>
+      </section>
+    )
+  }
+}
+
+export default ResultList;
